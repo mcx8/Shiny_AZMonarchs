bf0b4f25f62b53bb26c70abc0c05a2e9171256ac
Maxine Cruz <tmcruz@email.arizona.edu>
2022-10-11 08:55:58 -0700

    Updates

--- AZ_Monarchs_map/app.R
+++ AZ_Monarchs_map/app.R
@@ -1,47 +1,118 @@
 # Maxine Cruz
 # tmcruz@arizona.edu
 # Created: September 23, 2022
-# Last edited: September 23, 2022
+# Last edited: October 10, 2022
 # Version: RStudio 2021.09.0+351 "Ghost Orchid" Release
 
+# Purpose:
+  # Generates an R Shiny app for AZ Monarch data from GBIF and iNaturalist
+  # Interactive - shows and plots available data by month on map
+
+##############################################################################
+# Load libraries
+##############################################################################
 
 library(shiny)
+library(semantic.dashboard)
+library(leaflet)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+##############################################################################
+# Load monarch_data.csv
+##############################################################################
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+monarchs <- read.csv("AZ_Monarchs_map/data/monarch_data.csv")
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+
+##############################################################################
+# Define UI (User Interface)
+##############################################################################
+
+ui <- dashboardPage(
+  
+  # Title of page (shows up on browser tab)
+  title = "Arizona Monarchs",
+  
+  # Add header stuff
+  dashboardHeader(
+    
+    color = "yellow", inverted = TRUE,
+    
+    title = span(
+      tags$img(src="monarch-butterfly-ge3a41ab2f_1920.jpg", width = '100%'), 
+      column(12, class="title-box", 
+             tags$h1(class="primary-title", style='margin-top:10px;', 
+                     "ARIZONA MONARCH OCCURENCES"), 
+             tags$h2(class="primary-subtitle", style='margin-top:10px;', 
+                     "Data from January 2000 - December 2022")))
+    ),
+  
+  # Add content to main body of page
+  dashboardBody(
+    
+    fluidRow(
+    # Add interactive content
+    
+    # First item
+    box(
+      h3("Welcome!"),
+      helpText("Insert note here"),
+    
+    # Second item
+      checkboxGroupInput("checkGroup", 
+                         h3("Select data"), 
+                         choices = list("Choice 1" = 1, 
+                                        "Choice 2" = 2, 
+                                        "Choice 3" = 3),
+                         selected = 1),
+    
+    # Third item
+      sliderInput("slider",
+                  "Date:",
+                  min = as.Date("2000-01-01"),
+                  max = as.Date("2022-12-31"),
+                  value = as.Date("2010-01-01"),
+                  timeFormat="%b %Y"),
+      textOutput("SliderText"),
+    
+    # Fourth item
+      h3("End Notes"),
+      helpText("Insert note here")
+    ),
+    
+    # Add map
+    box(tags$style(type = "text/css", 
+                   "#map {height: calc(100vh - 80px) !important;}"),
+        leafletOutput("map"))
     )
+    
+    )
+  
 )
 
-# Define server logic required to draw a histogram
+
+##############################################################################
+# Define server logic
+##############################################################################
+
 server <- function(input, output) {
+  
+  output$map <- renderLeaflet({ leaflet() %>%
+      setView(lng = -111.682299, lat = 34.496789, zoom = 6.4) %>%
+      addProviderTiles("Esri.WorldStreetMap") %>%
+      addCircles(
+        data = monarchs,
+        color = "#000000",
+        fillColor = "#ffffff",
+        fillOpacity = 0.5)
+  })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
+
+##############################################################################
 # Run the application 
+##############################################################################
+
 shinyApp(ui = ui, server = server)
+
--- pull_Monarchs.R
+++ pull_Monarchs.R
@@ -2,46 +2,121 @@
 # tmcruz@arizona.edu
 
 # Created: September 23, 2022
-# Last edited: September 23, 2022
+# Last edited: October 10, 2022
 # Version: RStudio 2021.09.0+351 "Ghost Orchid" Release
 
 # Purpose:
   # Load libraries for downloading data
-  # Download Danaus plexippus (Monarch) data from GBIF
-  # Clean data for use in R Shiny
+  # Download Danaus plexippus (Monarch) data from GBIF and iNaturalist
+  # Clean data
+  # Save data for use in R Shiny
+  # Inspect data to determine settings in R Shiny
 
 ##############################################################################
 # Load libraries
 ##############################################################################
 
 library("spocc")
-library("rgbif")
+library("dplyr")
+library("ggplot2")
+library("rinat")
+library("rnaturalearth")
+library("lubridate")
 
 
 ##############################################################################
-# Collect occurrence data from GBIF
+# Collect occurrence data from GBIF and iNaturalist
 ##############################################################################
 
-# This takes a very long time, would be good to find a faster way
-D_plex <- occ_data(taxonKey = 5133088, 
-                   country = "US",
-                   limit = 500000)
+# Pull data from GBIF
+mdata <- occ(query = "Danaus plexippus",
+            from = "gbif",
+            gbifopts = list(country = "US",
+                            stateProvince = "Arizona",
+                            year = "2000, 2022"),
+            limit = 1100)
 
-# Records found [489570] 
-# Records returned [500] 
-# Therefore, we change the limit to 500000 to account for all observations
+# Searched: gbif
+# Occurrences - Found: 1,038, Returned: 500
+# Search type: Scientific
+# gbif: Danaus plexippus (500)
+
+# Pull data from iNaturalist
+inat <- get_inat_obs(query = "Danaus plexippus",
+                     quality = "research",
+                     place_id = 40,
+                     maxresults = 1000)
+
+# Without maxresults = 1000, the default maximum is 100
 
 
 ##############################################################################
 # Clean data
 ##############################################################################
 
-# Filter to years 2000-2022
+# Isolate GBIF data from other stuff
+mdata <- mdata$gbif$data$Danaus_plexippus
+
+# Store N/A values under a different dataframe
+mdata_na <- mdata[is.na(mdata$individualCount), ]
+
+# Remove N/A values in individualCount
+mdata <- mdata[!is.na(mdata$individualCount), ]
+
+# Keep necessary columns
+mdata <- select(mdata,
+                c(longitude, latitude, year, month, day))
+
+mdata_na <- select(mdata_na,
+                   c(longitude, latitude, year, month, day))
+
+inat <- select(inat,
+               c(longitude, latitude, observed_on))
+
+# iNat does not have year, month, and day separated
+inat <- inat %>% 
+  mutate(observed_on = ymd(observed_on)) %>% 
+  mutate_at(vars(observed_on), funs(year, month, day)) %>%
+  select(longitude, latitude, year, month, day)
+
+# Add new column to dataframes designating the type
+mdata$dataname <- "gbif_filtered"
+mdata_na$dataname <- "gbif_with_na"
+inat$dataname <- "inat"
+
+# Combine all into one dataframe
+monarch_data <- rbind(mdata, mdata_na, inat)
 
 
-# Filter to observations in Arizona
+##############################################################################
+# Save data
+##############################################################################
+
+# Save as .csv
+write.csv(monarch_data, "AZ_Monarchs_map/data/monarch_data.csv")
 
 
-# Remove duplicates
+##############################################################################
+# Check for correctness
+##############################################################################
 
+# Sets up map stuff
+# world <- ne_countries(scale = "medium", returnclass = "sf")
+# class(world)
+
+# Plot on map
+# sample <- ggplot(data = world) +
+#  geom_sf() +
+#  coord_sf(xlim = c(-115.9, -108.15), ylim = c(30.5, 37.85)) +
+#  geom_point(data = mdata,
+#             aes(x = longitude, y = latitude),
+#             position = position_jitter(width = 0.1, height = 0.1),
+#             colour = "red") +
+#  geom_point(data = inat,
+#             aes(x = longitude, y = latitude),
+#             position = position_jitter(width = 0.1, height = 0.1),
+#             colour = "blue") +
+#  borders("state") +
+#  theme_bw()
+  
 
